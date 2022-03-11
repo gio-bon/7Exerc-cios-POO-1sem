3070539b0f2e01e5e224ddbce57a0ae077d84934
Giovani Bontempo <injuriae+github@gmail.com>
2022-03-11 13:33:48 -0300

    teste nos módulos

--- EX7/Program.cs
+++ EX7/Program.cs
@@ -1,11 +1,23 @@
 ﻿using System;
+using Ex7.Classes;
+
 namespace MyApp // Note: actual namespace depends on the project name.
 {
   internal class Program
   {
     static void Main(string[] args)
     {
-      
+      Gerente heitor = new Gerente("Heitor", 33, 10000);
+      Console.WriteLine($"{heitor.Idade}");
+
+      Console.WriteLine($"Bonificação Gerente {heitor.Bonificacao()}"); 
+      Vendedor charlinho = new Vendedor("Charlinho", 22, 5000);
+
+      Console.WriteLine($"Bonificação Vendedor {charlinho.Bonificacao()}");
+
+      Supervisor joselito = new Supervisor("Joselito", 44, 6500);
+
+      Console.WriteLine($"Bonificação Supervisor {joselito.Bonificacao()}");
     }
   }
 }
